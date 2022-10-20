ad84628f2f2a1b8484290d2bb49bb0c0013cf57c
Enzo Samudio <enzosamudio@gmail.com>
2022-10-19 23:15:33 -0300

    Delete environment.prod.ts

--- src/environments/environment.prod.ts
+++
@@ -1,9 +0,0 @@
-export const environment = {
-  production: true,
-  baseApiUrl: 'https://portfolio-enzos-be.herokuapp.com/api/',
-  baseApiImages: 'https://firebasestorage.googleapis.com/v0/b/portfolio-fb9ed.appspot.com/o/images%2F',
-  apiKey: "AIzaSyCLhctqZ4ZjWg5c2BYw4YelEqLxX3v8Cro",
-  authDomain: "portfolio-fb9ed.firebaseapp.com",
-  projectId: "portfolio-fb9ed",
-  storageBucket: "portfolio-fb9ed.appspot.com"
-};
